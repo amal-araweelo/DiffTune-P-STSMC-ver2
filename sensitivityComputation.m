314277e10ab7944be6d1816b04eac7a175ae6d66
elb <s214680@dtu.dk>
2024-06-05 12:54:43 +0200

    added theta_r_dot to sensitivity computations

--- sensitivityComputation.m
+++ sensitivityComputation.m
@@ -7,23 +7,23 @@
 % Xref: desired/reference state
 % u: torque command (output of controller, input to system)
 % J_m: moment of inertia (from motor mechanical parameters)
-% k: controller gains (k_vec = [k1;k2;k_pos])
+% theta: controller gains (k_vec = [k1;k2;k_pos])
 % dt: taken from simulation parameter
 % dXdphi and dudphi: calculated sensitivities
 
-function [dXdphi,dudphi] = sensitivityComputation(sensitivity,X,Xref,u,J_m,k,dt)
+function [dXdphi,dudphi] = sensitivityComputation(sensitivity,X,Xref,theta_r_dot,u,param,theta,dt)
 
 % evaluate the Jacobians
-dfdX = grad_f_X_fcn(X,u,dt,J_m);
+dfdX = grad_f_X_fcn(X,u,dt,param);
 dfdX = full(dfdX);    % full() converts sparse matrix to full matrix (see https://se.mathworks.com/help/matlab/ref/full.html)
 
-dfdu = grad_f_u_fcn(X,u,dt,J_m);
+dfdu = grad_f_u_fcn(X,u,dt,param);
 dfdu = full(dfdu);
 
-dhdX = grad_h_X_fcn(X,Xref,k,J_m);
+dhdX = grad_h_X_fcn(X,Xref,theta_r_dot,theta,param);
 dhdX = full(dhdX);
 
-dhdtheta = grad_h_theta_fcn(X,Xref,k,J_m);
+dhdtheta = grad_h_theta_fcn(X,Xref,theta_r_dot,theta,param);
 dhdtheta = full(dhdtheta);
 
 % assemble the Jacobians to compute the sensitivity
