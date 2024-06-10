1ad523b699baec0e655f9468bfbc028205b098e7
elb <s214680@dtu.dk>
2024-06-10 15:24:11 +0200

    Changed Xref(4) to Xref

--- controller.m
+++ controller.m
@@ -17,43 +17,40 @@
 % ud: containing motor/load angular velocity/position (4outputs)
 
 function ud = controller(X, Xref, k_vec, theta_r_dot, theta_r_2dot, J_m, N, dt) % t for time
-    global v;
+global v;
 
-    % Controller gains
-    k1 = k_vec(1);
-    k2 = k_vec(2);
-    k_pos = k_vec(3);
+% Controller gains
+k1 = k_vec(1);
+k2 = k_vec(2);
+k_pos = k_vec(3);
 
-    % States
-    omega_m = X(1);
-    omega_l = X(2);
-    theta_l = X(4);
-    theta_r = Xref(4);
-    
-    % Controllers
- 
-    % P-controller
-    omega_r = k_pos * (theta_r - theta_l) + N * theta_r_dot;
-    omega_r_dot = k_pos * (theta_r_dot - omega_l) + N * theta_r_2dot;
-   
-    % PI-controller
-    % u = int((omega_r - omega_m) * k_vel * 1/tau_i) + (omega_r - omega_m) * k_vel + diff(omega_r) * J_m;
+% States
+omega_m = X(1);
+omega_l = X(2);
+theta_l = X(4);
+theta_r = Xref;
 
-    % STSMC controller
-    s = omega_m - omega_r; % Error
-    v_dot = -k2 * sgn_approx(100*s);
-    
-    if (isempty(v)) % initialise v to zero in first iteration
-        v = 0;
-    end
-    v = v + v_dot * dt;
+% Controllers
 
-    u_smc = -k1 * sqrt(abs(s)) * sgn_approx(s) + v;
-    % fprintf('omega_r_dot = %d \n', omega_r_dot);
-    u = u_smc + J_m * omega_r_dot;
-    % fprintf('u = %d \n', u);
+% P-controller
+omega_r = k_pos * (theta_r - theta_l) + N * theta_r_dot;
+omega_r_dot = k_pos * (theta_r_dot - omega_l) + N * theta_r_2dot;
 
-    % Output
-    ud = u;
-    % fprintf('ud = %d \n', ud);
+% STSMC controller
+s = omega_m - omega_r; % Error
+v_dot = -k2 * sgn_approx(100*s);
+
+if (isempty(v)) % initialise v to zero in first iteration
+    v = 0;
+end
+v = v + v_dot * dt;
+
+u_smc = -k1 * sqrt(abs(s)) * sgn_approx(s) + v;
+% fprintf('omega_r_dot = %d \n', omega_r_dot);
+u = u_smc + J_m * omega_r_dot;
+% fprintf('u = %d \n', u);
+
+% Output
+ud = u;
+% fprintf('ud = %d \n', ud);
 end 
